e1880c9072854122554b69fcd80dae3f41465bd4
marano <thiagomarano@gmail.com>
2014-06-16 23:05:35 -0300

    Squbes baseline implementation.

---
+++ Assets/Scripts/CameraPositionScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPositionScript : MonoBehaviour 
+{	
+	private Transform player;
+	
+	void Awake ()
+	{
+		player = GameObject.Find("Player").transform;
+	}
+
+	void LateUpdate ()
+	{
+		float speed = 5f;
+		float cameraHeight = 17f;
+
+		float xTarget = player.position.x;
+		float yTarget = player.position.y + cameraHeight;
+		float zTarget = player.position.z;
+		
+		transform.position = new Vector3 (xTarget, yTarget, zTarget);
+
+//		float xPositionModifier = 0f;
+//		float zPositionModifier = 0f;
+//
+//		float xPositionDifference = transform.position.x - player.position.x;
+//		float yPositionDifference = (transform.position.y + cameraHeight) - player.position.y;
+//		float zPositionDifference = transform.position.z - player.position.z;
+//
+//		float positionDifferenceTreshold = 1.5f;
+
+//		if (xPositionDifference > 0) {
+//			xPositionModifier = Mathf.Min(positionDifferenceTreshold, xPositionDifference);
+//		}
+//		else if (xPositionDifference < 0)
+//		{
+//			xPositionModifier = Mathf.Max(-positionDifferenceTreshold, xPositionDifference);
+//		}
+//
+//		if (zPositionDifference > 0) {
+//			zPositionModifier = Mathf.Min(positionDifferenceTreshold, zPositionDifference);
+//		}
+//		else if (xPositionDifference < 0)
+//		{
+//			zPositionModifier = Mathf.Max(-positionDifferenceTreshold, zPositionDifference);
+//		}
+
+
+
+//		float xTarget = Mathf.Lerp (transform.position.x, player.position.x + xPositionModifier, speed * Time.smoothDeltaTime);
+//		float yTarget = Mathf.Lerp (transform.position.y, player.position.y + cameraHeight, speed * Time.smoothDeltaTime);
+//		float zTarget = Mathf.Lerp (transform.position.z, player.position.z + zPositionModifier, speed * Time.smoothDeltaTime);
+		
+//		float xTarget = player.position.x + xPositionModifier;
+//		float yTarget = player.position.y + cameraHeight;
+//		float zTarget = player.position.z + zPositionModifier;
+
+
+//		Vector3 zero = Vector3.zero;
+//		transform.position = Vector3.SmoothDamp(transform.position, new Vector3 (xTarget, yTarget, zTarget), ref zero, 1f * Time.deltaTime);
+//		transform.position = Vector3.MoveTowards(transform.position, new Vector3 (xTarget, yTarget, zTarget), speed * Time.deltaTime);
+//		transform.position = new Vector3 (xTarget, yTarget, zTarget);
+		
+//		float speed2 = 10f;
+
+//		float xTarget = player.position.x;
+//		float yTarget = player.position.y + cameraHeight;
+//		float zTarget = player.position.z;
+////		
+//		transform.position = Vector3.MoveTowards(transform.position, new Vector3 (xTarget, yTarget, zTarget), speed * Time.deltaTime);
+
+//		rigidbody.velocity = new Vector3 (xVelocity, rigidbody.velocity.y, zVelocity);
+
+//		float xVelocity = (xPositionDifference * (-1f) * Time.smoothDeltaTime) + transform.position.x;
+//		float yVelocity = (yPositionDifference * (-1f) * Time.smoothDeltaTime) + transform.position.y;
+//		float zVelocity = (zPositionDifference * (-1f) * Time.smoothDeltaTime) + transform.position.z;
+//
+//		rigidbody.velocity = new Vector3 (xVelocity, yVelocity, zVelocity);
+	}
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour 
+{	
+	public float moveForce = 5f;
+	float maxSpeed = 5f;
+
+	void Update()
+	{
+	}
+
+	void FixedUpdate()
+	{
+		lookTowardCursor ();
+		moveFromInput ();
+		handleJump ();
+	}
+
+	void moveFromInput() {
+		float horizontalMove = CrossPlatformInput.GetAxis("Horizontal");
+		float verticalMove = CrossPlatformInput.GetAxis("Vertical");
+
+		float positiveHorizontalMove = horizontalMove < 0 ? horizontalMove * (-1) : horizontalMove;
+		float positiveVerticalMove = verticalMove < 0 ? verticalMove * (-1) : verticalMove;
+
+		float totalMovement = positiveHorizontalMove + positiveVerticalMove;
+
+		float xForce = horizontalMove * (totalMovement == 0f ? 0.7071f : (positiveHorizontalMove / totalMovement)) * moveForce;
+		float yForce = 0;
+		float zForce = verticalMove * (totalMovement == 0f ? 0.7071f : (positiveVerticalMove / totalMovement)) * moveForce;
+
+		if (xForce < 0 && rigidbody.velocity.x > 0) {
+			rigidbody.velocity = new Vector3 (0f, rigidbody.velocity.y, rigidbody.velocity.z);
+		}
+		else if (xForce > 0 && rigidbody.velocity.x < 0)
+		{
+			rigidbody.velocity = new Vector3 (0f, rigidbody.velocity.y, rigidbody.velocity.z);
+		}
+		
+		if (zForce < 0 && rigidbody.velocity.z > 0) {
+			rigidbody.velocity = new Vector3 (rigidbody.velocity.x, rigidbody.velocity.y, 0f);
+		}
+		else if (zForce > 0 && rigidbody.velocity.z < 0)
+		{
+			rigidbody.velocity = new Vector3 (rigidbody.velocity.x, rigidbody.velocity.y, 0f);
+		}
+
+		if (rigidbody.velocity.x > maxSpeed || rigidbody.velocity.x < -maxSpeed) {
+			xForce = 0;
+		}
+		
+		if (rigidbody.velocity.z > maxSpeed || rigidbody.velocity.z < -maxSpeed) {
+			zForce = 0;
+		}
+
+		rigidbody.AddForce (new Vector3(xForce, yForce, zForce), ForceMode.Impulse);
+	}
+
+	void handleJump()
+	{
+		float jumpForce = 10f;
+		RaycastHit hit;
+		Physics.Raycast (transform.position, transform.TransformDirection (Vector3.down), out hit, Mathf.Infinity);
+		print (hit.distance);
+		bool isGrounded = (hit.distance - ((gameObject.GetComponent<BoxCollider>().size.y / 2) + 0.5f)) <= 0f;
+
+		if (isGrounded && Input.GetButtonDown("Jump")) {
+			rigidbody.AddForce (new Vector3 (0, jumpForce, 0), ForceMode.Impulse);
+		}
+	}
+
+	void lookTowardCursor()
+	{
+		float speed = 5f; 
+
+		Plane playerPlane = new Plane(Vector3.up, transform.position);
+		Ray cameraRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		float distanceFromCameraToPlane;
+
+		if (playerPlane.Raycast (cameraRay, out distanceFromCameraToPlane))
+		{
+			Vector3 targetPosition = cameraRay.GetPoint(distanceFromCameraToPlane);
+			Quaternion targetRotation = Quaternion.LookRotation(targetPosition - transform.position);
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.smoothDeltaTime);
+		}
+	}
+}
