50634225399a570af13852ef296d8d75800a5466
marano <thiagomarano@gmail.com>
2014-06-17 09:48:06 -0300

    Started working on bullet.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public Rigidbody bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  if (Input.GetButtonDown ("Fire1"))
+	  {
+			float shootForce = 100f;
+
+			rigidbody.AddForce (new Vector3(1,2,3), ForceMode.Impulse);
+
+			GameObject bullet = (GameObject) Instantiate(bulletPrefab, transform.position, transform.rotation);
+//			rigidbody.addForce(new Vector3(Vector3.right, 0f, 0f), ForceMode.Impulse);
+			bullet.rigidbody.addForce(new Vector3(1,2,3), ForceMode.Impulse);
+
+	  }
+	}
+}
